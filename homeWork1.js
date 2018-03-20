ebe8d2736b4551b80eef5129090f066c446a9476
Vitaliy Levchenko <levchv@gmail.com>
2018-03-20 12:40:52 +0200

    home work 1 - done!

--- homeWork1.js
+++ homeWork1.js
@@ -1,39 +1,108 @@
 (function(){
 	if (!location.search) {
-		fetchPosts();
+		defaultFetch();
 		return;
 	}
 	
 	const params = location.search.slice(1).split('&').map(i => i.split('=')).reduce((res, i) => Object.assign(res, { [i[0]]: i[1] }), {});
 	if (!params || params.length === 0) {
-		fetchPosts();
+		defaultFetch();
 		return;
 	}
-	const paramInfo= [{key: 'postId', func: fetchPost}, {key: 'userId', func: fetchUserPosts}].find(i => params[i.key]);
+
+	const paramInfo= [{key: 'postId', func: fetchComments}, {key: 'userId', func: fetchUserPosts}].find(i => params[i.key]);
 	if (!paramInfo) {
-		fetchPosts();
+		defaultFetch();
 		return;
 	}
 	
 	const {key, func} = paramInfo;	
 	func(params[key]);
 	
-	function fetchPost(postId) {
-		fetchFromJsonPlaceHolder(`/comments?postId=${postId}`);
+	function defaultFetch() {
+		return fetchUsers().then(_ => fetchPosts());
 	}
-	
+
 	function fetchUserPosts(userId) {
-		fetchPosts(`userId=${userId}`);
+		return fetchPosts(`userId=${userId}`);
 	}
 	
 	function fetchPosts(filters) {
-		fetchFromJsonPlaceHolder(filters ? `/posts?${filters}` : '/posts');
+		return fetchEntities('posts', filters, showPosts);
 	}
 	
-	function fetchFromJsonPlaceHolder(path) {		
-		fetch(`https://jsonplaceholder.typicode.com${path}`)
+	function fetchUsers(filters) {
+		return fetchEntities('users', filters, showUsers);
+	}
+	
+	function fetchComments(postId) {
+		return fetchEntities('comments', `postId=${postId}`, showComments);
+	}
+	
+	function fetchEntities(name, filters, handler) {
+		return fetchFromJsonPlaceHolder(`/${name}${filters ? `?${filters}` : ''}`, handler);
+	}
+	
+	function fetchFromJsonPlaceHolder(path, handlerFn) {		
+		return fetch(`https://jsonplaceholder.typicode.com${path}`)
 			.then(response => response.json())
-			.then(json => console.log(json));
+			.then(result => { handlerFn(result); return result; });
+	}
+
+	function showPosts(posts){
+		showElements('Posts', posts, createPostElement);
+	}
+
+	function showUsers(users){
+		showElements('Users', users, createUserElement);
+	}
+
+	function showComments(comments){
+		showElements('Comments', comments, createCommentElement);
+	}
+
+	function showElements(header, elements, createElementFn){
+		const container = document.getElementById('content');
+		const h1 = document.createElement('h1');
+		h1.appendChild(document.createTextNode(header));
+		container.appendChild(h1);
+		elements.forEach(i => container.appendChild(createElementFn(i)));
+	}
+
+	function createPostElement(post){
+		const p = document.createElement('p');
+		const h2 = document.createElement('h2');
+		const a = document.createElement('a');
+		a.href = `?postId=${post.id}`;
+		a.appendChild(document.createTextNode(`${post.title}`));
+		h2.appendChild(a);
+		p.appendChild(h2);
+		const pre = document.createElement('pre');
+		pre.appendChild(document.createTextNode(`${post.body}`));
+		p.appendChild(pre)
+		return p;
+	}
+
+	function createUserElement(user){
+		const p = document.createElement('p');
+		const h2 = document.createElement('h2');
+		const a = document.createElement('a');
+		a.href = `?userId=${user.id}`;
+		a.appendChild(document.createTextNode(`${user.name}`));
+		h2.appendChild(a);
+		p.appendChild(h2);
+		return p;
+	}
+
+	function createCommentElement(comment){
+		const p = document.createElement('p');
+		const h2 = document.createElement('h2');
+		h2.appendChild(document.createTextNode(`${comment.name}`));
+		p.appendChild(h2);
+		const pre = document.createElement('pre');
+		pre.appendChild(document.createTextNode(`${comment.body}`));
+		p.appendChild(pre)
+		return p;
 	}
 	
 })();
