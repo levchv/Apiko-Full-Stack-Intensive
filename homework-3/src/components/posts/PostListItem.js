de5bddca1a2bfd076a240efa649b0affebc98404
Vitaliy <levchv@gmail.com>
2018-04-22 12:41:07 +0300

    home work 5 - done!

--- homework-3/src/components/posts/PostListItem.js
+++ homework-3/src/components/posts/PostListItem.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const PostListItem = ({title}) => (
+/*const PostListItem = ({title}) => (
     <li>{title}</li>
-);
+);*/
+
+class PostListItem extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.title !== this.props.title;
+    }
+
+    render() {
+        return (
+            <li>{this.props.title}</li>
+        )
+    }
+}
+
 
 PostListItem.propTypes = {
     title: PropTypes.string.isRequired
 }
 
-export default PostListItem;
+export default PostListItem; 
